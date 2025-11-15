1794e42b9db39f599c7d3ecdae903c2ed1b33baa
Ernesto Casaverde <u20221b657@upc.edu.pe>
2025-11-15 09:12:56 -0500

    fix: manual chunking to divide libraries

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,29 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: (id) => {
+          if (id.includes('node_modules')) {
+            if (id.includes('vue') || id.includes('vue-router')) {
+              return 'vue-vendor';
+            }
+            if (id.includes('primevue') || id.includes('primeicons')) {
+              return 'primevue-vendor';
+            }
+            if (id.includes('pinia')) {
+              return 'pinia-vendor';
+            }
+            if (id.includes('axios')) {
+              return 'axios-vendor';
+            }
+            return 'vendor';
+          }
+        }
+      }
+    }
+  },
   server: {
     port: 5173,
     proxy: {
